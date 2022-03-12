09968006d96fdf22ee9e992d74dc9d5287f7a2b0
Denis SOARES <soaresden@gmail.com>
2022-03-12 11:26:30 +0100

    icon

--- Retro Bat-O-era Mover/My Project/Settings.Designer.vb
+++ Retro Bat-O-era Mover/My Project/Settings.Designer.vb
@@ -88,9 +88,9 @@ Namespace My
     Friend Module MySettingsProperty
         
         <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.Retro_Bat_O_era_Mover.My.MySettings
+        Friend ReadOnly Property Settings() As Global.Retro_Bat_O_Cera_Mover.My.MySettings
             Get
-                Return Global.Retro_Bat_O_era_Mover.My.MySettings.Default
+                Return Global.Retro_Bat_O_Cera_Mover.My.MySettings.Default
             End Get
         End Property
     End Module
